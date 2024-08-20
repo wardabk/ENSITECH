3fb439fe2b5dd7c06533bd1a58f0288742bb82cc
glowriousmou <oumou.vero.sow@gmail.com>
2024-08-20 18:52:42 +0000

    Integration navigation menu

--- src/components/menu/js/menu.js
+++ src/components/menu/js/menu.js
@@ -69,9 +69,21 @@ function loadMenu(path) {
                 </a>
                 </li>
                 <li class="nav-item">
+                <a class="nav-link " href=${path.etudiantPage}>
+                   <i class="fa-solid fa-graduation-cap"></i>
+                    <span>Etudiant</span>
+                </a>
+                </li>
+                <li class="nav-item">
                 <a class="nav-link " href=${path.coursPage}>
                     <i class="fa-solid fa-book"></i>
                     <span>Cours</span>
+                </a>
+                </li>
+                 <li class="nav-item">
+                <a class="nav-link " href=${path.enseignantPage}>
+                  <i class="fa-solid fa-users"></i>
+                    <span>Enseignant</span>
                 </a>
                 </li>
             </aside>
