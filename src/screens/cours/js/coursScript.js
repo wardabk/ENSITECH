9f7061df69f9f713219489d8fba0701c1afb2a61
glowriousmou <oumou.vero.sow@gmail.com>
2024-08-22 18:11:12 +0000

    Module Cours: Ajout button Delete

--- src/screens/cours/js/coursScript.js
+++ src/screens/cours/js/coursScript.js
@@ -24,3 +24,30 @@
     }
     loadCoursTable();
 }) */
+
+  var selectedRow= null;
+  // Fonction alert
+
+  function showAlert(message, className){
+    const div= document.createElement("div");
+    div.className=`alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container= document.querySelector(".container");
+    const screenContainer= document.querySelector(".screenContainer");
+    container.insertBefore(div, screenContainer);
+    setTimeout(()=> document.querySelector(".alert").remove(),3000)
+
+  }
+
+  //supprimer un cours
+
+  document.querySelector("#coursList").addEventListener("click",(e) => {
+    
+    const target= e.target;
+    // console.log("ee",e.target);
+    if(target.classList.contains("delete")){
+      target.parentElement.parentElement.remove();
+      showAlert("Cours Supprimé", "danger");
+    }
+    // alert()
+  })
