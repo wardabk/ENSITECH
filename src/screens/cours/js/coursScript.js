2ed9f7bec443160874089a7d59686aeb5afc01ae
glowriousmou <oumou.vero.sow@gmail.com>
2024-08-23 09:39:29 +0000

    Module Cours : EV1-3: Création d'un cours

--- src/screens/cours/js/coursScript.js
+++ src/screens/cours/js/coursScript.js
@@ -25,29 +25,76 @@
     loadCoursTable();
 }) */
 
-  var selectedRow= null;
-  // Fonction alert
+var selectedRow = null;
+const LIST = "liste";
+const FORM = "Form";
+// Fonction alert
 
-  function showAlert(message, className){
-    const div= document.createElement("div");
-    div.className=`alert alert-${className}`;
-    div.appendChild(document.createTextNode(message));
-    const container= document.querySelector(".container");
-    const screenContainer= document.querySelector(".screenContainer");
-    container.insertBefore(div, screenContainer);
-    setTimeout(()=> document.querySelector(".alert").remove(),3000)
+function showAlert(message, className, place) {
+  const div = document.createElement("div");
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(message));
 
+  let container = document.querySelector(".container");
+  let screenContainer = document.querySelector(".screenContainer");
+  if (place === FORM) {
+    container = document.querySelector(".modal-body");
+    screenContainer = document.querySelector("#coursForm");
   }
 
-  //supprimer un cours
+  container.insertBefore(div, screenContainer);
+  setTimeout(() => document.querySelector(".alert").remove(), 3000)
 
-  document.querySelector("#coursList").addEventListener("click",(e) => {
-    
-    const target= e.target;
-    // console.log("ee",e.target);
-    if(target.classList.contains("delete")){
-      target.parentElement.parentElement.remove();
-      showAlert("Cours Supprimé", "danger");
+}
+//Vider la liste des champs
+
+function clearFields() {
+  document.querySelector("#theme").value = "";
+  document.querySelector("#nbreHeure").value = "";
+}
+
+/// Créer un cours
+document.querySelector("#coursForm").addEventListener("submit", (e) => {
+  console.log("ddd")
+  e.preventDefault();
+
+  //Données du formulaire
+  const theme = document.querySelector('#theme').value;
+  const nbreHeure = document.querySelector('#nbreHeure').value;
+  console.log("ddd", theme)
+  //validation des données
+  if (theme === "" || nbreHeure === "") {
+    showAlert("Veuillez remplir tous les champs", "danger", FORM)
+  } else {
+    if (selectedRow === null) {
+      const list = document.querySelector("#coursList");
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td> ${theme}</td>
+        <td> ${nbreHeure}</td>
+         <td>
+            <a href="" class="btn btn-success btn-sm detail">Détails</a>
+            <a href="" class="btn btn-warning btn-sm edit">Modifier</a>
+            <a href="#" class="btn btn-danger btn-sm delete">Supprimer</a>
+          </td>
+        `;
+        list.appendChild(row);
+        selectedRow=null;
+        showAlert("Cours enregistré avec succès", "success",LIST)
     }
-    // alert()
-  })
+    }
+
+
+});
+
+//supprimer un cours
+document.querySelector("#coursList").addEventListener("click", (e) => {
+
+  const target = e.target;
+  // console.log("ee",e.target);
+  if (target.classList.contains("delete")) {
+    target.parentElement.parentElement.remove();
+    showAlert("Cours Supprimé", "danger", LIST);
+  }
+  // alert()
+})
